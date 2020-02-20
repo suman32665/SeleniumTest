a3829822e48c2f0fdb0410fcb636539bd3c2a215
suman32665 <somonme@gmail.com>
2020-02-20 14:59:06 +0545

    [Test] Updated test to run using testng.xml

--- src/test/java/SeleniumSupport/Helper.java
+++ src/test/java/SeleniumSupport/Helper.java
@@ -1,10 +1,5 @@
 package SeleniumSupport;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -12,33 +7,51 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-public class Helper extends Locators{
+public class Helper extends Locators {
 	WebDriver driver;
 	WebDriverWait wait;
 	JavascriptExecutor jsx;
+
 	public Helper(WebDriver d) {
 		driver = d;
 		wait = new WebDriverWait(driver, 10);
 		jsx = (JavascriptExecutor) driver;
 	}
-	
+
 	public void GetURL() {
 		driver.get(url);
 	}
+
 	public void Click_JavaScriptAlerts() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(JavaScriptAlerts))).click();
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(JavaScriptAlerts))).click();
+			System.out.println("Java Script Alert is verified and clicked");
+		} catch (Exception e) {
+			Assert.fail(e.toString());
+		}
 	}
 
 	public void Click_JSConfirm() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(JSConfirm))).click();		
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(JSConfirm))).click();
+			System.out.println("JS Confirm is verified and clicked");
+		} catch (Exception e) {
+			Assert.fail(e.toString());
+		}
 	}
-	
+
 	public void Click_OK() {
-		driver.switchTo().alert().accept();
+		try {
+			driver.switchTo().alert().accept();
+			System.out.println("OK Button is verified and clicked");
+		}catch (Exception e) {
+			Assert.fail(e.toString());
+		}
 	}
-	
+
 	public void Check_Result(String ExpectedText) {
 		String ActualText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Result))).getText();
 		Assert.assertEquals(ActualText, ExpectedText);
+		System.out.println("Result text is Validated");
 	}
 }
